d64adadc711398880ede07914843b7997846a4b6
DevMovegun <developer_movegun@naver.com>
2023-01-14 02:37:07 +0900

    Chap_01 자료형과 변수

---
+++ JavaWorkspace/src/chap_01/TypeCasting.java
@@ -0,0 +1,21 @@
+package chap_01;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+
+        // int 형을 String 으로 변환하는 2가지 방법
+        String s1 = String.valueOf(100);
+        String s2 = Integer.toString(101);
+        System.out.println("s1 = " + s1 + " s2 = " + s2); // s1 = 100 s2 = 101
+
+        // double 형을 String 으로 변환하는 2가지 방법
+        s1 = String.valueOf(98.8);
+        s2 = Double.toString(98.9);
+        System.out.println("s1 = " + s1 + " s2 = " + s2); // s1 = 98.8 s2 = 98.9
+
+        // String을 숫자로 변환
+        int i = Integer.parseInt("100");
+        double d = Double.parseDouble("98.8");
+        System.out.println("i = " + i + " d = " + d); // i = 100 d = 98.8
+    }
+}
---
+++ JavaWorkspace/src/quiz/Quiz_01.java
@@ -0,0 +1,19 @@
+package quiz;
+
+public class Quiz_01 {
+    public static void main(String[] args) {
+
+        // 버스 번호
+        String busName = "상암08";
+        // 남은 시간
+        int waitTime = 3;
+        // 남은 거리
+        double remainDistance = 1.8;
+
+        // 결과 출력
+        System.out.println(busName + " 버스");
+        System.out.println("약 " + waitTime + "분 후 도착");
+        System.out.println("남은 거리 " + remainDistance + "km");
+
+    }
+}
